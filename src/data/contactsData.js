cead81f095816c23264624c750e79504536c35ac
Juliano Boese <juliano.boese@gmail.com>
2022-04-22 01:36:16 -0300

    refactor(data): :rotating_light: fix lint errors in data files

--- src/data/contactsData.js
+++ src/data/contactsData.js
@@ -1,7 +1,9 @@
 export const contactsData = {
-    email: 'janedoe.test@gmail.com',
-    phone: '+915588776600',
-    address: 'Menlo Park, California, United States - 673822 ',
+  email: 'janedoe.test@gmail.com',
+  phone: '+915588776600',
+  address: 'Menlo Park, California, United States - 673822 ',
 
-    sheetAPI: ''
-}
+  sheetAPI: '',
+};
+
+export default contactsData;
--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -1,24 +1,25 @@
- export const skillsData = [
-    'HTML',
-    'Fastify',
-    'Blender',
-    'Figma',
-    'Javascript',
-    'CSS',
-    'React',
-    'Django',
-]
+export const skillsData = [
+  'HTML',
+  'Fastify',
+  'Blender',
+  'Figma',
+  'Javascript',
+  'CSS',
+  'React',
+  'Django',
+];
+
+export default skillsData;
 
 // Choose your skills from below. Make sure it's in the same format and spelled correctly.
 // Couldn't find the required skills? Raise an issue on github at https://github.com/hhhrrrttt222111/developer-portfolio/issues/new
 
-
 // AVAILABLE SKILLS
 
-/* 
+/*
     HTML
     CSS
-    JS 
+    JS
     React
     Next JS
     Nuxt JS
