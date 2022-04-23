c5a56f1bca1cafd9ede5b5c1c84f19188a396b41
Juliano Boese <juliano.boese@gmail.com>
2022-04-23 09:43:45 -0300

    feat(data): :busts_in_silhouette: add my education and experience

--- src/data/educationData.js
+++ src/data/educationData.js
@@ -1,23 +1,23 @@
 export const educationData = [
   {
     id: 1,
-    institution: 'Marble Hills Middle School',
-    course: 'Higher Secondary Education',
-    startYear: '2007',
-    endYear: '2015',
+    institution: 'Pontifícia Universidade Católica do Rio Grande do Sul',
+    course: 'Chemical Engineering',
+    startYear: '2010',
+    endYear: '2016',
   },
   {
     id: 2,
-    institution: 'Somerset School of Science',
-    course: 'Bachelor of Technology',
+    institution: 'Fundação Getúlio Vargas',
+    course: 'MBA Business Management',
     startYear: '2015',
     endYear: '2019',
   },
   {
     id: 3,
-    institution: 'Hawking University, Cambridge',
-    course: 'Master of Technology',
-    startYear: '2019',
+    institution: 'Trybe',
+    course: 'Full Stack Web Development',
+    startYear: '2021',
     endYear: 'Present',
   },
 ];
