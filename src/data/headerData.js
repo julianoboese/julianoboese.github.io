d4c914056ad900ca5704413cd54e9a653324ae6d
Juliano Boese <juliano.boese@gmail.com>
2022-04-22 23:18:51 -0300

    feat(data): :busts_in_silhouette: add my personal data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,10 +1,11 @@
 import resume from '../assets/pdf/resume.pdf';
+import profileImage from '../assets/jpg/profile-image.jpg';
 
 export const headerData = {
-  name: 'Jane Doe',
+  name: 'Juliano Boese',
   title: 'Web Developer',
   desciption: "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
-  image: 'https://raw.githubusercontent.com/PhantomScript/asset-container/main/developer-portfolio/landingImg.png',
+  image: profileImage,
   resumePdf: resume,
 };
 
