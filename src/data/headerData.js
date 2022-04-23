582bfbf73990ccc41b7710726887e1095175e861
Juliano Boese <juliano.boese@gmail.com>
2022-04-23 00:23:44 -0300

    refactor(data): :pencil2: fix typo in header data

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,7 @@ import profileImage from '../assets/jpg/profile-image.jpg';
 export const headerData = {
   name: 'Juliano Boese',
   title: 'Web Developer',
-  desciption: "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
+  description: "There wasn't a bird in the sky, but that was not what caught her attention. It was the clouds. The deep green that isn't the color of clouds, but came with these. She knew what was coming and she hoped she was prepared. ",
   image: profileImage,
   resumePdf: resume,
 };
