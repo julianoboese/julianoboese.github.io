82a8bc54fb5604b3c5f3832df803571139088f69
Juliano Boese <juliano.boese@gmail.com>
2022-06-05 14:03:11 -0300

    feat(projects): :sparkles: add projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,84 +1,34 @@
 import one from '../assets/svg/projects/one.svg';
 import two from '../assets/svg/projects/two.svg';
 import three from '../assets/svg/projects/three.svg';
-import four from '../assets/svg/projects/four.svg';
-import five from '../assets/svg/projects/five.svg';
-import six from '../assets/svg/projects/six.svg';
-import seven from '../assets/svg/projects/seven.svg';
-import eight from '../assets/svg/projects/eight.svg';
 
 export const projectsData = [
   {
     id: 1,
-    projectName: 'Employee Management',
-    projectDesc: 'This project aims to build an employee management system that consolidates all information of a company',
-    tags: ['React', 'CSS', 'Material Ui'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    projectName: 'Recipes App',
+    projectDesc: 'App for mobile devices where you can search for meals and drinks recipes, save your favorites and several functionalities.',
+    tags: ['React', 'Context API', 'Styled Components'],
+    code: 'https://github.com/julianoboese/recipes-app',
+    demo: 'https://recipes-app-julianoboese.vercel.app/',
     image: one,
   },
   {
     id: 2,
-    projectName: 'Railway Tracking',
-    projectDesc: 'This project is a railway tracking system that allows users to obtain relevant information about train timing.',
-    tags: ['Django', 'Tailwind CSS'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    projectName: 'Trivia',
+    projectDesc: 'Trivia game where you can customize several settings and choose from a range of topics to play with.',
+    tags: ['React', 'Redux', 'Material UI'],
+    code: 'https://github.com/julianoboese/trivia-react-redux',
+    demo: 'https://trivia-react-redux-julianoboese.vercel.app/',
     image: two,
   },
   {
     id: 3,
-    projectName: 'Weather App',
-    projectDesc: 'Weather forecast systems and applications predict weather conditions based on multiple parameters.',
-    tags: ['Django', 'CSS', 'Material Ui'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
+    projectName: 'Pokétrunfo',
+    projectDesc: 'App where you can create a deck of Pokémon and play Super Trunfo (Ace Trumps).',
+    tags: ['React', 'CSS'],
+    code: 'https://github.com/julianoboese/tryunfo',
+    demo: 'https://julianoboese.github.io/tryunfo/',
     image: three,
-  },
-  {
-    id: 4,
-    projectName: 'Android Patient Tracker',
-    projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-    tags: ['Flutter', 'Firebase'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    image: four,
-  },
-  {
-    id: 5,
-    projectName: 'E-Commerce App',
-    projectDesc: 'A Simple E-commerce application',
-    tags: ['React Native', 'Firebase'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    image: five,
-  },
-  {
-    id: 6,
-    projectName: 'Uber Lite',
-    projectDesc: 'Uber clone',
-    tags: ['Flutter'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    image: six,
-  },
-  {
-    id: 7,
-    projectName: 'Stock Market App',
-    projectDesc: 'A simple stock market API app',
-    tags: ['React', 'Redux', 'Bootstrap'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    image: seven,
-  },
-  {
-    id: 8,
-    projectName: 'Car Pooling System',
-    projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-    tags: ['Flutter', 'React'],
-    code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    image: eight,
   },
 ];
 
