ae3179a98530797245818c62b1dbec1108a0b583
Juliano Boese <juliano.boese@gmail.com>
2022-07-09 11:45:30 -0300

    refactor(data): :busts_in_silhouette: update my description

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -5,14 +5,14 @@ export const headerData = {
   pt: {
     name: 'Juliano Boese',
     title: 'Desenvolvedor Web',
-    description: 'Sou um desenvolvedor front-end que adora criar sites e aplicações web acessíveis, escrever testes e ajudar pessoas a aprenderem a programar. Recentemente, tenho construído aplicações web com React e provedores serverless, como Vercel e Netlify.',
+    description: 'Sou um desenvolvedor Full-Stack que adora criar aplicações web acessíveis, escrever testes e ajudar pessoas a aprenderem a programar. Recentemente, tenho construído aplicações web com React, Node.js e provedores serverless, como Vercel e Heroku.',
     image: profileImage,
     resumePdf: resume,
   },
   en: {
     name: 'Juliano Boese',
     title: 'Web Developer',
-    description: 'I’m a front-end developer who loves building user-friendly websites and web apps, enjoys writing tests and loves helping people learn how to code. Most recently, I’ve been building web apps with React and serverless providers like Vercel and Netlify.',
+    description: "I'm a Full-Stack developer who loves building user-friendly web apps, enjoys writing tests and loves helping people learn how to code. Most recently, I've been building web apps with React and Node.js and serverless providers like Vercel and Heroku.",
     image: profileImage,
     resumePdf: resume,
   },
